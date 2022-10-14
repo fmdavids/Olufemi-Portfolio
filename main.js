34d21f8631db6b771b1f76a75e7814df8cda02d1
fmdavids <fmproade@gmail.com>
2022-10-14 01:47:07 +0100

    scrotoview and mail added

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 // document.querySelectorAll()
-let txt = "HTML Email, WordPress and Web App Developer.";
-
+let txt = "React, Node. See my projects.";
 
 let i = "";
-speed= 200;
+speed = 200;
 
 function typeWriter() {
   if (i < txt.length) {
@@ -13,34 +12,37 @@ function typeWriter() {
   }
 }
 
+// function scrollToWorks() {
+//   setTimeout(scrollToWorks, 15000)
+//   document.getElementById("works").scrollIntoView({
+//     behavior: "smooth",
+//   });
+// }
+
+setTimeout(() => document.getElementById("works").scrollIntoView({
+      behavior: "smooth",
+    }), 6000)
+
 let copyRightYear = new Date().getFullYear();
 
-document.getElementById('year').innerHTML = copyRightYear;
+document.getElementById("year").innerHTML = copyRightYear;
 
-let mobileSideNave = document.querySelector(".sideNavBar")
-let mobileSideMenu = document.querySelector('.navMenu');
-document.getElementById("mobileNav").addEventListener("click", function(){
+let mobileSideNave = document.querySelector(".sideNavBar");
+let mobileSideMenu = document.querySelector(".navMenu");
+document.getElementById("mobileNav").addEventListener("click", function () {
+  mobileSideNave.classList.add("sideNavBar2");
+});
 
-  mobileSideNave.classList.add('sideNavBar2')
-})
+mobileSideMenu.addEventListener("click", function () {
+  mobileSideNave.classList.remove("sideNavBar2");
+});
 
-mobileSideMenu.addEventListener("click", function(){
-  mobileSideNave.classList.remove('sideNavBar2')
-  })
-  
 let ul = document.querySelector("ul");
 let li = document.querySelectorAll("li");
 
-li.forEach(element => {
-  element.addEventListener("click", function(){
-    // navMenu.classList.remove('.active')
-    ul.querySelector(".active").classList.remove('active')
-    
-    element.classList.add('active')
-    
-  })
-  
-})
-
-
-
+li.forEach((element) => {
+  element.addEventListener("click", function () {
+    ul.querySelector(".active").classList.remove("active");
+    element.classList.add("active");
+  });
+});
